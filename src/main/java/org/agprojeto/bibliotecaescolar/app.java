5e02606087f75f1d6a4ab2578a530d2f4d76a268
Anderson da Silva Santos <148016944+dogbardoDev@users.noreply.github.com>
2025-01-28 19:37:53 -0300

    Update app.java

--- src/main/java/org/agprojeto/bibliotecaescolar/app.java
+++ src/main/java/org/agprojeto/bibliotecaescolar/app.java
@@ -12,7 +12,7 @@ public class app extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(app.class.getResource("TelaCadastroUsuario.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Cadastro De Usuario!");
         stage.setScene(scene);
         stage.show();
     }
